1701c53bbbb5c0eda078452e9739e3b39852e721
Charles Seaton <seatonct@gmail.com>
2024-02-17 18:50:38 -0600

    added random number feature

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io::{self};
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
